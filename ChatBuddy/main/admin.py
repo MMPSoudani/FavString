aa752a858a93261f37df7e6d864fded0b456b559
MMPSoudani <mmpsudani@gmail.com>
2022-03-01 17:55:53 +0330

    Created the models
    	- created the models Profile, Topic, Room and Message
    	- modified the model User to use email instead of username
    	- created a signal so that each time a user is created a profile gets created
    	- registerd the models in admin.py

--- ChatBuddy/main/admin.py
+++ ChatBuddy/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Profile, Topic, Room, Message
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Topic)
+admin.site.register(Room)
+admin.site.register(Message)
