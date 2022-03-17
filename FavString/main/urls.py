7273147acc31e456b45bad7ca1031f77f97102ff
MMPSoudani <mmpsudani@gmail.com>
2022-03-18 01:14:37 +0330

    created the required URL links for the views in the urls.py file

--- FavString/main/urls.py
+++ FavString/main/urls.py
@@ -1,5 +1,16 @@
 from django .urls import path
+from main.views import (
+    HomeView, LoginView, RegisterView, LogoutView, AddStringView,
+    StringOverView,
+)
 
 
 app_name = "main"
-urlpatterns = []
+urlpatterns = [
+    path(route="", view=HomeView.as_view(), name="home"),
+    path(route="login/", view=LoginView.as_view(), name="login"),
+    path(route="register/", view=RegisterView.as_view(), name="register"),
+    path(route="logout/", view=LogoutView.as_view(), name="logout"),
+    path(route="add_string/", view=AddStringView.as_view(), name="add_string"),
+    path(route="string/<str:id>/overview/", view=StringOverView.as_view(), name="string_overview"),
+]
