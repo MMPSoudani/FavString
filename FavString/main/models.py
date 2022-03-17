3a7c70d51225c7e4669dca9813158911357336b9
MMPSoudani <mmpsudani@gmail.com>
2022-03-18 00:29:38 +0330

    modified the models.py file
    	- there are now three models: User, AuthUser and String
    	- the User model is custom made without using any django's user model
    		- it has the boolean attribute "is_super" to check if a user is a supervisor
    		- when registering a new user, the "is_super" can be checked or not
    	- the AuthUser contains a table of all the loged in users
    		- if a user logs out, they are removed from this table
    	- the String model contains a many-to-many relationship for all the users that favored that string
    		- when a superuser adds a new string, a signal is sent to calculate its weight

--- FavString/main/models.py
+++ FavString/main/models.py
@@ -1,35 +1,37 @@
 from django.db import models
+from datetime import datetime
 
 
 class User(models.Model):
     username = models.CharField(max_length=150)
     email = models.EmailField()
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
     password = models.CharField(max_length=30)
     is_super = models.BooleanField(default=False)
+    joined_at = models.DateTimeField(null=True, blank=True)
+    last_login = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.username
 
 
+class AuthUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_authenticated = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}"
+
+
 class String(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     string = models.TextField()
-    weight = models.IntegerField()
+    weight = models.IntegerField(blank=True, null=True, default=0)
+    favored_by = models.ManyToManyField(User, related_name="favored_by")
     created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"{self.string[:20]}"
     
 
     class Meta:
-        ordering = ["-updated", "-created"]
-
-
-class FavoriteString(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    strings = models.ManyToManyField(String, blank=True)
-
-    def __str__(self) -> str:
-        return f"{self.user.username}'s favorite strings"
+        ordering = ["-created"]
