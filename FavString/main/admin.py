d8da0c37bc877b427b501d5e4ea90b564cd7c597
MMPSoudani <mmpsudani@gmail.com>
2022-03-18 00:43:33 +0330

    registered the models from main.models into admin.py

--- FavString/main/admin.py
+++ FavString/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import User, AuthUser, String
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(AuthUser)
+admin.site.register(String)
